3e261ef6235f9d6f8ef2a8436cad941b245b05d8
Jemuel Cadayona <jemuelcadayona3@gmail.com>
2025-09-04 20:56:53 +0800

    update

--- resources/views/pages/admin/ics-print.blade.php
+++ resources/views/pages/admin/ics-print.blade.php
@@ -2,7 +2,7 @@
     <div class="flex min-h-screen flex-col">
         
         <div class="flex-1">
-             <iframe src="/admin/ics-form/{{ $encrypted_id }}" width="100%" height="1000" frameborder="0"></iframe>
+             <iframe src="{{ route('admin.ics-form', ['encrypted_id' => $encrypted_id]) }}" width="100%" height="1000" frameborder="0"></iframe>
         </div>
 
         <x-footer class="mt-auto" />
