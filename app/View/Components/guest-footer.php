d376d8aedd5317a13a0afe2e7500cd19cd7f3226
Jemuel Cadayona <cdynjm@gmail.com>
2025-09-15 08:11:43 +0800

    update

--- app/View/Components/guest-footer.php
+++ app/View/Components/guest-footer.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class guest-footer extends Component
+class GuestFooter extends Component
 {
     /**
      * Create a new component instance.
