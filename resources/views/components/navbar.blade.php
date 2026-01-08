c17050df68984deb177c6918c368cfa7c7a5c532
Jemuel Cadayona <cdynjm@gmail.com>
2026-01-08 08:50:49 +0800

    updated and modified some changes

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,14 +13,14 @@
             ['label' => 'Home', 'icon' => 'solar:home-2-line-duotone', 'route' => 'admin.dashboard'],
             ['label' => 'ARE', 'icon' => 'solar:document-add-line-duotone', 'route' => 'admin.are-records'],
             ['label' => 'ICS', 'icon' => 'solar:document-text-line-duotone', 'route' => 'admin.ics-records'],
-            ['label' => 'RPCPPE', 'icon' => 'mingcute:document-2-fill', 'route' => 'admin.rpcppe-records'],
+            ['label' => 'RPCPPE', 'icon' => 'solar:file-text-line-duotone', 'route' => 'admin.rpcppe-records'],
             ['label' => 'Codes', 'icon' => 'lets-icons:folder-alt-duotone', 'route' => 'admin.accounts-code'],
         ],
         'office' => [
             ['label' => 'Home', 'icon' => 'solar:home-2-line-duotone', 'route' => 'office.dashboard'],
             ['label' => 'ARE', 'icon' => 'solar:document-add-line-duotone', 'route' => 'office.are-records'],
             ['label' => 'ICS', 'icon' => 'solar:document-text-line-duotone', 'route' => 'office.ics-records'],
-            ['label' => 'RPCPPE', 'icon' => 'mingcute:document-2-fill', 'route' => 'office.rpcppe-records'],
+            ['label' => 'RPCPPE', 'icon' => 'solar:file-text-line-duotone', 'route' => 'office.rpcppe-records'],
         ],
         default => [],
     };
